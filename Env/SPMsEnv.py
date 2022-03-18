755cb67341c6c293bdddff3e84f8721c5da552f0
Livioni <livion_i@icloud.com>
2022-03-18 16:52:27 +0800

    根据原文更新了环境
    下一步是用PPO方法训练

--- Env/SPMsEnv.py
+++ Env/SPMsEnv.py
@@ -13,26 +13,25 @@ class SPMsEnv(gym.Env):
         ## 初始化动作和状态的上下限
         self.items_num = 5     #假设item有n个
         self.agent_num = 3     #假设agent有m个
-        self.min_agent = 0
-        self.max_agent = 4
-        self.delta = 0
+        self.delta = 0         #调整生成agent的valuation function的correlations
 
-        self.rou_agents = np.array([i for i in range(self.agent_num)],dtype=np.int16)
-        self.rou_items = np.array([i for i in range(self.items_num)],dtype=np.int16)
-        self.X = np.zeros([self.agent_num,self.items_num],dtype=np.int16)
-        self.tau = np.zeros(self.agent_num,dtype=np.float32)
+        self.rou_agents = np.ones(self.agent_num,dtype=np.int16)  #剩余的agent bool编码，固定self.agent_num个状态，1为还在，0为不在
+        self.rou_items = np.ones(self.items_num,dtype=np.int16)   #剩余的item bool编码，固定self.item_num个状态，1为还在，0为不在  
+        self.allocation_matrix = np.zeros([self.agent_num,self.items_num],dtype=np.int16) #分配矩阵 bool编码，固定（self.agent_num * self.item_num)维，1为还在，0为不在  
+        self.tau = np.zeros(self.agent_num,dtype=np.float32) #转账矩阵 
         self.agent = np.array(-1,dtype=np.int16)  #初始化观察agent #初始化观察agent
-        self.price = np.ones(self.items_num,dtype=np.float32) 
+        self.price = np.ones(self.items_num,dtype=np.float32) #要价矩阵
         self.socialwelfare = 0    
         self.viewer = None
 
     def return_dim_info(self):
-        self.state_dim = 1 + self.items_num + self.items_num * self.agent_num
+        #总的状态数量为：当前选择的agent（1维）+物品报价（self.item_num维）+当前选择agent的分配矩阵（self.item_num维）+剩余的agent编码（self.agent_num维）+剩余的item编码（self.item_num维）
+        self.state_dim = 1 + self.items_num + self.items_num + self.agent_num + self.items_num
         self.action_dim = self.agent_num+self.items_num
         return self.state_dim, self.action_dim
 
     def check_done(self):
-        return True if len(self.rou_agents) == 0 or len(self.rou_items) == 0 else False
+        return True if np.all(self.rou_agents == 0) or np.all(self.rou_items == 0) else False
 
     def return_socialwelfare(self):
         return self.socialwelfare
@@ -40,41 +39,46 @@ class SPMsEnv(gym.Env):
     def return_tau_agent(self):
         return self.rou_agents
 
+    def return_allocation_matrix(self):
+        return self.allocation_matrix       
+
     def step(self, action):
         self.agent = int(action[0])
         self.price = action[1:]
         for ele in range(len(self.price)):
-            if ele in self.rou_items:
+            if self.rou_items[ele] == 1:
                 continue
             else:
-                self.price[ele] = 99
+                self.price[ele] = 99 #如果商品已经售出则标价99 agent买不起。
 
         for i in range(self.items_num):
             if self.valuationF[self.agent][i] > self.price[i]:
-                self.X[self.agent][i] = 1
-                index = np.argwhere(self.rou_items == i)
-                self.rou_items = np.delete(self.rou_items,index)
+                self.allocation_matrix[self.agent][i] = 1
+                self.rou_items[i] = 0
 
-        self.socialwelfare += self.X[self.agent].dot(self.valuationF[self.agent].T)
-        self.tau[self.agent] = self.X[self.agent].dot(self.price.T)
+        self.socialwelfare += self.allocation_matrix[self.agent].dot(self.valuationF[self.agent].T)
+        self.tau[self.agent] = self.allocation_matrix[self.agent].dot(self.price.T)
         self.agent = np.array([self.agent],dtype=np.int16)
 
-        index = np.argwhere(self.rou_agents == self.agent)
-        self.rou_agents = np.delete(self.rou_agents,index)
+        self.rou_agents[self.agent] = 0
         done = self.check_done()
+        #收益函数：如果是最后一步，则计算整个socialwelfare，如果不是最后一步，则为0；
         if done:
             reward = self.socialwelfare 
         else: 
             reward = 0
+
         self.state1 = np.array([action],dtype=np.float32)
-        self.state = np.hstack((self.state1.flatten(),self.X.flatten()))
+        self.state = np.hstack((self.state1.flatten(),self.allocation_matrix[self.agent].flatten()))  
+        self.state = np.hstack((self.state,self.rou_agents.flatten()))    
+        self.state = np.hstack((self.state,self.rou_items.flatten()))    
         return self.state, reward, done, {}
 
     def reset(self):
         ##初始化信息
-        self.rou_agents = np.array([i for i in range(self.agent_num)],dtype=np.int16)
-        self.rou_items = np.array([i for i in range(self.items_num)],dtype=np.int16)
-        self.X = np.zeros([self.agent_num,self.items_num],dtype=np.int16)
+        self.rou_agents = np.ones(self.agent_num, dtype=np.int16)
+        self.rou_items = np.ones(self.items_num ,dtype=np.int16)
+        self.allocation_matrix = np.zeros([self.agent_num,self.items_num],dtype=np.int16)
         self.tau = np.zeros(self.agent_num,dtype=np.float32)
         self.socialwelfare = 0 
         ##初始化效用函数
@@ -87,9 +91,15 @@ class SPMsEnv(gym.Env):
         #初始化状态
         self.agent = np.array(-1,dtype=np.int16)  #初始化观察agent
         self.price = np.ones(self.items_num,dtype=np.float32) #初始化报价
-        self.X = np.zeros([self.agent_num,self.items_num],dtype=np.int16) #初始化allocation
+        self.allocation_matrix = np.zeros([self.agent_num,self.items_num],dtype=np.int16) #初始化allocation
+        #状态表示：【当前选择的agent，报价1，报价2，报价3，报价4，报价5，当前agent是否选择物品1，
+        #当前agent是否选择物品2，当前agent是否选择物品3，当前agent是否选择物品4，当前agent是否选择物品5，
+        #agent1是否未访问，agent2是否未访问，agent3是否未访问，item1是否还在，item2是否还在，item3是否还在，
+        #item4是否还在，item5是否还在】
         self.state = np.hstack((self.agent,self.price.flatten()))
-        self.state = np.hstack((self.state,self.X.flatten()))
+        self.state = np.hstack((self.state,self.allocation_matrix[self.agent].flatten()))    
+        self.state = np.hstack((self.state,self.rou_agents.flatten()))    
+        self.state = np.hstack((self.state,self.rou_items.flatten()))           
         return self.state
 
     def render(self):
